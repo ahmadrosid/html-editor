f9520a388134bbc033e81b97caf3f6ecd303f71f
ahmadrosid <alahmadrosid@gmail.com>
2024-07-08 09:13:32 +0700

    chore: tidy up

--- src/routes/Share.svelte
+++ src/routes/Share.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { createQuery } from '@tanstack/svelte-query'
+  import { createQuery } from "@tanstack/svelte-query";
 
   import { sourceCode } from "../lib/store";
   import { fetchCode } from "../lib/api";
@@ -8,38 +8,36 @@
   export let id = "";
 
   const query = createQuery({
-    queryKey: ['getCode', id],
+    queryKey: ["getCode", id],
     queryFn: () => fetchCode(id),
     enabled: id !== "",
-  })
+  });
 
   $: if ($query.isSuccess) {
-    console.log('Query completed successfully! (reactive)')
-    console.log($query.data)
+    console.log("Query completed successfully! (reactive)");
+    console.log($query.data);
     let data = $query.data.data;
     sourceCode.update(() => ({
-        html: data.html,
-        css: data.css,
-        js: data.js,
-      }));
+      html: data.html,
+      css: data.css,
+      js: data.js,
+    }));
   }
 </script>
 
 {#if $query.isLoading}
-<div class="grid place-content-center absolute inset-0 z-10 bg-white">
+  <div class="grid place-content-center absolute inset-0 z-10 bg-white">
     <div class="flex gap-2 text-gray-600">
-    <Loader2 size={24} class="animate-spin" />
-    <span>Loading the content...</span>
+      <Loader2 size={24} class="animate-spin" />
+      <span>Loading the content...</span>
     </div>
-</div>
+  </div>
 {:else if $query.isError}
-<div class="grid place-content-center absolute inset-0 z-10 bg-white">
+  <div class="grid place-content-center absolute inset-0 z-10 bg-white">
     <div class="flex gap-2 text-gray-600">
-        <span>{$query.error.message}</span>
+      <span>{$query.error.message}</span>
     </div>
-    </div>
+  </div>
 {:else if $query.isSuccess}
-<div class="relative">
-    <Home />
-</div>
+  <Home />
 {/if}
